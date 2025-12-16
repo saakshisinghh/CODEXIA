f10c157825d93c822d95d2ebe0edace8bcf4381d
Sakshi Singh <singhsakshii.9192@gmail.com>
2025-12-16 17:06:44 +0530

    home-page design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,28 @@
-import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton } from '@clerk/clerk-react'
-import { Route, Routes } from 'react-router'
-import HomePage from './pages/HomePage'
+import { useUser } from "@clerk/clerk-react";
+import { Navigate, Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
+
+import { Toaster } from "react-hot-toast";
+import DashboardPage from "./pages/DashboardPage";
+
 
 function App() {
+    const { isSignedIn, isLoaded } = useUser();
+
+  // this will get rid of the flickering effect
+  if (!isLoaded) return null;
   return (
+    <>
     
      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/problem" element={<div>PROBLEM PAGE</div>} />
+          <Route path="/" element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />} />
+        <Route path="/dashboard" element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />} />
+
+           <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />} />
       </Routes>
+
+        <Toaster toastOptions={{ duration: 3000 }} />
+    </>
     
   )  
 }
