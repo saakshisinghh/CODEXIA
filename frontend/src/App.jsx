b29537a264c9d62372c845b5aa42374f0d61428f
Sakshi Singh <singhsakshii.9192@gmail.com>
2025-12-16 17:39:24 +0530

    Problems-page desgined and added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useUser } from "@clerk/clerk-react";
 import { Navigate, Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
+import ProblemsPage from "./pages/ProblemsPage";
 
 import { Toaster } from "react-hot-toast";
 import DashboardPage from "./pages/DashboardPage";
