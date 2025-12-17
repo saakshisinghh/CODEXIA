4aba22d7fa2794b0cfebe33a142acb684b3ea94c
Sakshi Singh <singhsakshii.9192@gmail.com>
2025-12-17 17:04:22 +0530

    FINAL ERROR SOLVING AND SESSION CREATION

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,11 +5,13 @@ import cors from "cors";
 import {serve} from "inngest/express";
 import {clerkMiddleware} from '@clerk/express';
 
+
+
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
 import {  inngest,functions} from "./lib/inngest.js";
 import chatRoutes from "./routes/chatRoutes.js";
-
+import sessionRoutes from "./routes/sessionRoute.js";
 const app = express();
 
 // ESM-safe __dirname. On Render this becomes: /opt/render/project/src/backend/src
@@ -25,7 +27,7 @@ app.use(clerkMiddleware());
 // routes
 app.use("/api/inngest", serve ({client:inngest , functions}))
 app.use("/api/chat",chatRoutes)
-
+app.use("/api/sessions", sessionRoutes);
 
 // example 
 app.get("/health", (req, res) => {
